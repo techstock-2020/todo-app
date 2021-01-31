6ca8d9af2c7550aa17f514bc3204d233f134d86a
Jithin <jithinone08@gmail.com>
2021-01-31 17:08:32 +0530

    logout functionality Google login

--- security-service/src/main/java/com/todo/config/WebSecurityConfiguration.java
+++ security-service/src/main/java/com/todo/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,6 +22,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.exceptionHandling(e -> e  //to configure endpoints to respond with a 401 instead of the default behavior of redirecting to a login page.
 									.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 							  )
+			.csrf(c -> c   //csrf later check based on the Fronend Ui technology
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				 )
+			.logout(l -> l
+					.logoutSuccessUrl("/").permitAll()
+				    )
 			.oauth2Login();
 	}
 }
--- security-service/src/main/java/com/todo/controller/SecurityController.java
+++ security-service/src/main/java/com/todo/controller/SecurityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
 	@GetMapping("/user")
-	public Map<String,Object> user(@AuthenticationPrincipal OAuth2User principal){
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
 		return Collections.singletonMap("name", principal.getAttribute("name"));
 	}
 }
