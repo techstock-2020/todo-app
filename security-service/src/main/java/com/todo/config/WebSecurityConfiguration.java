98556299fee8625ec7e1a03352500c3fc93b2c35
Jithin <jithinone08@gmail.com>
2021-01-31 16:13:46 +0530

    ui for with a link added
    Security cinfiguration added

--- security-service/src/main/java/com/todo/config/WebSecurityConfiguration.java
+++ security-service/src/main/java/com/todo/config/WebSecurityConfiguration.java
@@ -1,42 +1,26 @@
-//package com.todo.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.security.SecurityProperties.Filter;
-//import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.oauth2.client.OAuth2ClientContext;
-//import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-//import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-//import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-//
-//@EnableOAuth2Client
-//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	private OAuth2ClientContext oauth2ClientContext;
-//
-//	@Bean
-//	@ConfigurationProperties("security.oauth2.google.client")
-//	public AuthorizationCodeResourceDetails google() {
-//		return new AuthorizationCodeResourceDetails();
-//	}
-//
-//	@Bean
-//	@ConfigurationProperties("security.oauth2.google.resource")
-//	public OAuth2ResourceServerProperties googleResource() {
-//		return new OAuth2ResourceServerProperties();
-//	}
-//
-//	private Filter ssoGoogleFilter() {
-//		OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter(
-//				"/login/google");
-//		OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oauth2ClientContext);
-//		
-//		googleFilter.setRestTemplate(googleTemplate);
-//		googleFilter.setTokenServices(new UserInfoTokenServices(googleResource().getUserInfoUri(), google().getClientId()));
-//		return googleFilter;
-//	}
-//}
+package com.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(a -> a
+									.antMatchers("/", "/error", "/webjars/**")
+									.permitAll()
+									.anyRequest()
+									.authenticated()
+							  )
+			.exceptionHandling(e -> e  //to configure endpoints to respond with a 401 instead of the default behavior of redirecting to a login page.
+									.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+							  )
+			.oauth2Login();
+	}
+}
